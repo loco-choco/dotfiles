9dfc7ba4b6af0e887a0ab3cb1dca1de368df83be
Locochoco <ivanrwpf@gmail.com>
2023-05-28 17:45:22 -0300

    initial commit

---
+++ system/configuration.nix
@@ -0,0 +1,254 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let
+  nixpkg-23-05 = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/23.05-pre)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader
+  boot.loader = {
+    #systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot/efi";
+    timeout = 1;
+    grub = {
+      enable = true;
+      version = 2;
+      devices = ["nodev"];
+      efiSupport = true;
+      useOSProber = true;
+      default = "saved";
+      splashMode = "normal";
+    };
+  };
+
+
+  networking.hostName = "locochoco"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.locochoco = {
+    isNormalUser = true;
+    description = "Locochoco";
+    extraGroups = [ "networkmanager" "wheel" "dialout" "plugdev" ];
+    packages = with pkgs; [
+      firefox
+      kate
+    ];
+  };
+
+  #rtl-sdr
+  hardware.rtl-sdr.enable = true;
+
+  # Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "locochoco";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ]; #enable flakes and experimental nix commands
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     xclip
+     #git
+     #kitty
+     discord
+     peek
+     inkscape
+     openscad
+     #nixpkg-23-05.freecad
+     gimp
+     godot_4
+     wine
+     unzip
+     p7zip
+     lutris
+     mono
+     rtw89-firmware # driver for intelbras wifi dongle
+     #video editing
+     shotcut
+     obs-studio
+     vlc
+     #facul
+     #kicad #usando ltspice com wine
+     ghdl
+     gtkwave
+     quartus-prime-lite
+     #ngspice #vem com kicad
+     #rust
+     rustc
+     cargo
+     #arduino
+     arduino-cli
+     #pic programming
+     #sdcc
+     #pk2cmd
+     #building pkgs
+     nix-prefetch-github
+     #vr
+     #nixpkg-23-05.monado
+     #modding
+     jetbrains.rider
+     dotnet-sdk_7
+     msbuild
+     #nixpkg-23-05.unityhub
+     #avalonia-ilspy
+     #music creation
+     famistudio
+     #sdr
+     (nixpkg-23-05.gnuradio3_8.override {
+       extraPackages = with nixpkg-23-05.gnuradio3_8Packages; [
+         osmosdr
+       ];
+     })
+     gqrx
+  ];
+
+  environment.localBinInPath = true;
+  environment.variables.EDITOR = "nvim";
+  programs = {
+     steam = with pkgs; {
+  	enable = true;
+  	remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  	dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	package = steam.override { # steam beta fix
+          extraProfile = ''
+            export GSETTINGS_SCHEMA_DIR="${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}/glib-2.0/schemas/"
+          '';
+        };
+     };
+     kdeconnect.enable = true;
+  };
+
+  #Home Manager
+  home-manager.users.locochoco = (import ./home.nix);
+
+  #change default shell to zsh
+  users.defaultUserShell = pkgs.zsh;
+  
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # NVIDIA drivers are unfree
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+  hardware.nvidia.modesetting.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  };  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
---
+++ system/home.nix
@@ -0,0 +1,100 @@
+{ pkgs, ... }:
+{
+  home.username = "locochoco";
+  home.homeDirectory = "/home/locochoco";
+
+  home.packages = with pkgs; [
+    gnupg
+    git-crypt
+    pinentry-qt
+  ];
+
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "qt";
+  };
+
+
+  programs.kitty = {
+    enable = true;
+    theme = "Space Gray Eighties";
+    keybindings = {
+      "ctrl+k" = "kitten mykitten.py";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "ShoosGun";
+    userEmail = "ivanrwpf@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+      nvim-treesitter.withAllGrammars
+      plenary-nvim
+      gruvbox-material
+      mini-nvim
+      #openscad-nvim #on nixpkgs 23.05
+    ];
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    enableVteIntegration = true;
+    autocd = true;
+    shellAliases = {
+      "icat" = "kitty +kitten icat";
+      "d" = "kitty +kitten diff";
+      "edconf" = "nvim /etc/nixos/configuration.nix";
+      "edhome" = "nvim /etc/nixos/home.nix";
+      "nixrebuild" = "nixos-rebuild switch";
+      "nixclean" = "nix-collect-garbage";
+      "sedconf" = "sudo nvim /etc/nixos/configuration.nix";
+      "sedhome" = "sudo nvim /etc/nixos/home.nix";
+      "snixrebuild" = "sudo nixos-rebuild switch";
+      "snixclean" = "sudo nix-collect-garbage";
+    };
+    plugins = [
+      #{
+      #  # will source zsh-autosuggestions.plugin.zsh
+      #  name = "zsh-autosuggestions";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "zsh-users";
+      #    repo = "zsh-autosuggestions";
+      #    rev = "v0.4.0";
+      #    sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      #  };
+      #}
+    ];
+    prezto = {
+      enable = true;
+      prompt.theme = "paradox";
+      pmodules = [ 
+        "environment"
+        "terminal"
+        "editor"
+        "history"
+        "directory"
+        "spectrum"
+        "utility"
+	"git"
+        "completion"
+        "prompt"
+      ];
+    };
+   }; 
+}
