0676ef332ee026ac41517c248e9e4f3bf8142ecc
Locochoco <ivanrwpf@gmail.com>
2023-05-29 09:01:43 -0300

    added terminal utils

--- system/configuration.nix
+++ system/configuration.nix
@@ -117,11 +117,15 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     #terminal utils
      wget
      xclip
-     #git
-     #kitty
+     mpv
+     ranger
+     termpdfpy
+     mdcat
+     w3m
+     #random stuff
      discord
      peek
      inkscape
