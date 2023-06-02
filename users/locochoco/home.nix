bfea72a75b04a96f7b38db7de5541c1937b01469
Locochoco <ivanrwpf@gmail.com>
2023-06-02 14:57:27 -0300

    added newsboat

--- users/locochoco/home.nix
+++ users/locochoco/home.nix
@@ -27,7 +27,16 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
+  
+  programs.newsboat = {
+    enable = true;
+    autoReload = true;
+    urls = [
+      {
+        url = "https://xeiaso.net/blog.rss";
+      }
+    ];
+  };
   programs.gpg = {
     enable = true;
   };
