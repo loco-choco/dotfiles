960b16c2917ffd6f68ec38a471b82c34ea7e6ff1
Locochoco <ivanrwpf@gmail.com>
2023-06-22 09:13:27 -0300

    change thiagokokada nixpkgs to the master branch

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
   
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
-    nixpkgs-opentabletdriver.url = "github:thiagokokada/nixpkgs/bump-opentabletdriver";
+    nixpkgs-master.url = "nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ow-mod-man.url = "github:ShoosGun/ow-mod-man-flake/main";
     ow-mod-man.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { nixpkgs, home-manager, ow-mod-man, nixpkgs-opentabletdriver, ... }:
+  outputs = { nixpkgs, home-manager, ow-mod-man, nixpkgs-master, ... }:
   let
     system = "x86_64-linux";
     
@@ -18,8 +18,8 @@
       owmods-gui = ow-mod-man.packages.${system}.owmods-gui;
     };
     opentabletdriver-overlay = final: prev: {
-      opentabletdriver-pkgs = import nixpkgs-opentabletdriver { system = final.system; };
-      fixed-opentabletdriver = final.opentabletdriver-pkgs.opentabletdriver.overrideAttrs (old: {
+      pkgs-master = import nixpkgs-master { system = final.system; };
+      fixed-opentabletdriver = final.pkgs-master.opentabletdriver.overrideAttrs (old: {
         src = prev.fetchFromGitHub {
           owner = "loco-choco";
           repo = "OpenTabletDriver";
