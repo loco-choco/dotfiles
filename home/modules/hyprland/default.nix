9f03cf9e11f0ff78c961d3664c2adb32052212d1
locochoco <ivanrwpf@gmail.com>
2023-12-29 10:39:09 -0300

    fixed wallpaper not changing on window move and workspace 10 lacking walpaper

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -54,6 +54,7 @@
               "$mod, ${ws}, workspace, ${toString (x + 1)}"
               "$mod, ${ws}, exec, swww img ~/.dotfiles/home/modules/hyprland/wallpaper/rw-region-${toString (x + 1)}.png --transition-type right --transition-duration 0 --transition-step 255"
               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, exec, swww img ~/.dotfiles/home/modules/hyprland/wallpaper/rw-region-${toString (x + 1)}.png --transition-type right --transition-duration 0 --transition-step 255"
             ]
           )
           10)
