5a210aad3d5e4dea3128aff41cb1fca0e249a3fb
Locochoco <ivanrwpf@gmail.com>
2024-04-14 16:49:20 -0300

    fix missing bluetooth

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   hardware.bluetooth.powerOnBoot = true;
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
   
   networking.networkmanager.enable = true;
   networking.firewall = {
