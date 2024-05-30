ca9969895002b01177027ff09de04ee2455b55f5
Locochoco <ivanrwpf@gmail.com>
2024-05-30 13:04:35 -0300

    'fix' for nvidia issues in hyprland

--- system/modules/drivers/nvidia.nix
+++ system/modules/drivers/nvidia.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }: {
   services.xserver.videoDrivers = [ "nvidia" ];
-
   hardware.opengl.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }
