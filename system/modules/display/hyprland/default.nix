173018de9bd4ad8c3cc5ccf7c5cdde49f9be5322
Locochoco <ivanrwpf@gmail.com>
2024-06-21 19:14:41 -0300

    readded portals and added greeter

--- system/modules/display/hyprland/default.nix
+++ system/modules/display/hyprland/default.nix
@@ -1,5 +1,23 @@
 { config, pkgs, ... }: {
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    autorun = false;
+    exportConfiguration = true;
+    displayManager.startx.enable = true;
+  };
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --greeting 'Welcome aboard captain!' --remember"; 
+        user = "greeter";
+      };
+    };
+  };
 }
