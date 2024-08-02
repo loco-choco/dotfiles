aeadd568854ebcb23a0f080e7b2f4b78ec39cecf
loco-choco <contact@locochoco.dev>
2024-08-01 21:11:36 -0300

    added HP DeskJet printer

--- system/modules/cups.nix
+++ system/modules/cups.nix
@@ -1,6 +1,21 @@
 { config, pkgs, ... }:
 {
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "HP_DeskJet_2676";
+        location = "Home";
+        deviceUri = "usb://HP/DeskJet%202600%20series?serial=BR77RFB3NH06P5&interface=1";
+        model = "HP/hp-deskjet_plus_6000_series.ppd.gz";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+  };
+
   services.printing.cups-pdf = {
     enable = true;
     instances.pdf = {
