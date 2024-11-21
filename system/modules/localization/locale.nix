637976d29c7eb892c5f8e31b4ab6af93c931725d
Locochoco <ivanrwpf@gmail.com>
2024-11-21 11:35:20 -0300

    changed locale to FR

--- system/modules/localization/locale.nix
+++ system/modules/localization/locale.nix
@@ -2,14 +2,25 @@
   time.timeZone = "America/Sao_Paulo";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
+    LC_ADDRESS = "fr-FR.UTF-8";
+    LC_IDENTIFICATION = "fr-FR.UTF-8";
+    LC_MEASUREMENT = "fr-FR.UTF-8";
+    LC_MONETARY = "fr-FR.UTF-8";
+    LC_NAME = "fr-FR.UTF-8";
+    LC_NUMERIC = "fr-FR.UTF-8";
+    LC_PAPER = "fr-FR.UTF-8";
+    LC_TELEPHONE = "fr-FR.UTF-8";
+    LC_TIME = "fr-FR.UTF-8";
   };
+  # i18n.extraLocaleSettings = {
+  #   LC_ADDRESS = "pt_BR.UTF-8";
+  #   LC_IDENTIFICATION = "pt_BR.UTF-8";
+  #   LC_MEASUREMENT = "pt_BR.UTF-8";
+  #   LC_MONETARY = "pt_BR.UTF-8";
+  #   LC_NAME = "pt_BR.UTF-8";
+  #   LC_NUMERIC = "pt_BR.UTF-8";
+  #   LC_PAPER = "pt_BR.UTF-8";
+  #   LC_TELEPHONE = "pt_BR.UTF-8";
+  #   LC_TIME = "pt_BR.UTF-8";
+  # };
 }
