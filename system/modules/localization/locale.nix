76ad0d09fba4c2e2c8a95de21b0f065133b4b658
Locochoco <ivanrwpf@gmail.com>
2024-12-05 22:54:53 -0300

    changed default locale to FR

--- system/modules/localization/locale.nix
+++ system/modules/localization/locale.nix
@@ -2,15 +2,15 @@
   time.timeZone = "America/Sao_Paulo";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr-FR.UTF-8";
-    LC_IDENTIFICATION = "fr-FR.UTF-8";
-    LC_MEASUREMENT = "fr-FR.UTF-8";
-    LC_MONETARY = "fr-FR.UTF-8";
-    LC_NAME = "fr-FR.UTF-8";
-    LC_NUMERIC = "fr-FR.UTF-8";
-    LC_PAPER = "fr-FR.UTF-8";
-    LC_TELEPHONE = "fr-FR.UTF-8";
-    LC_TIME = "fr-FR.UTF-8";
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
   };
   # i18n.extraLocaleSettings = {
   #   LC_ADDRESS = "pt_BR.UTF-8";
