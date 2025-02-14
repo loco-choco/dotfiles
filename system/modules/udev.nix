7ff179de6e312772234f39db5f938e8230f75ec5
loco-choco <contact@locochoco.dev>
2025-02-14 10:22:24 -0300

    added udev example pkg

--- system/modules/udev.nix
+++ system/modules/udev.nix
@@ -15,22 +15,15 @@ in {
   '';
   services.udev.packages = [
     pkgs.openfpgaloader
-    (pkgs.writeTextFile {
-      name = "via-bot";
-      text = ''
-        # Rules to filter and symlink the robot usb sensors and other devices
-        # Right Motor
-        KERNEL=="ttyUSB[0-9]*", SUBSYSTEM=="tty", KERNELS=="1-[1-9]*.1", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="right-motor"
-        # Left Motor
-        KERNEL=="ttyUSB[0-9]*", SUBSYSTEM=="tty", KERNELS=="1-[1-9]*.4", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="left-motor"
-        # UBLOX
-        KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="tty", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1546", ATTRS{idProduct}=="01a8", SYMLINK+="ublox-gps"
-        # Cube 
-        #KERNEL=="ttyACM[0-9]*", SUBSYSTEM=="tty", SUBSYSTEMS=="usb", ATTRS{idVendor}=="", ATTRS{idProduct}=="", SYMLINK+="cube-orange"
-      '';
+    /* (pkgs.writeTextFile {
+         name = "via_udev";
+         text = ''
+           ...
+         '';
 
-      destination = "/etc/udev/rules.d/99-via-bot.rules";
-    })
+         destination = "/etc/udev/rules.d/99-via.rules";
+       })
+    */
   ];
 
 }
