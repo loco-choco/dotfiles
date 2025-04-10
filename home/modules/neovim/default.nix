f91b88b7375aed1a95a5ef5efc272bbc52da425b
Locochoco <ivanrwpf@gmail.com>
2025-04-10 11:26:58 -0300

    fixed broken autocomplete

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -12,6 +12,8 @@
     };
     plugins.lightline.enable = true;
     plugins.coq-nvim.enable = true;
+
+    ## LSP
     plugins.lsp = {
       enable = true;
       servers = {
@@ -52,10 +54,63 @@
       enable = true;
       package = pkgs.nixfmt-rfc-style;
     };
-    plugins.cmp-nvim-lsp.enable = true;
+
+    ## Auto Complete
+    # (from https://github.com/GaetanLepage/nix-config/blob/master/home/modules/tui/neovim/completion.nix)
+    opts.completeopt = [
+      "menu"
+      "menuone"
+      "noselect"
+    ];
+
+    plugins.luasnip.enable = true;
+
+    plugins.lspkind = {
+      enable = true;
+
+      cmp = {
+        enable = true;
+        menu = {
+          nvim_lsp = "[LSP]";
+          nvim_lua = "[api]";
+          path = "[path]";
+          luasnip = "[snip]";
+          buffer = "[buffer]";
+          neorg = "[neorg]";
+          nixpkgs_maintainers = "[nixpkgs]";
+        };
+      };
+    };
+
     plugins.cmp = {
       enable = true;
-      settings.completion.autocomplete = [ "TextChanged" ];
+
+      settings = {
+        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested.
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          { name = "neorg"; }
+          { name = "nixpkgs_maintainers"; }
+        ];
+      };
     };
   };
 }
