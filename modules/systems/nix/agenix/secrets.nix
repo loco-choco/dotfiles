6005d26c57c0f62514d2542bdb9c559090b26e24
loco-choco <contact@locochoco.dev>
2025-07-19 12:54:30 -0300

    added homepc keys

--- modules/systems/nix/agenix/secrets.nix
+++ modules/systems/nix/agenix/secrets.nix
@@ -1,7 +1,11 @@
 let
-  locopc =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKOXQlqmDdEpTUaeR2qBlOps/b6D4iK2f5F8SBU7mv0";
-  locoware =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW2w63E8Ek6d2uBgsck0/LNbCPWnZNDAmM/EF03sttq";
-  systems = [ locopc locoware ];
-in { "github-api.age".publicKeys = systems; }
+  homepc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzPLQIs7/fbV4X8tMJfx2Y6xAFE3UoLBRbpNmyHhbZD";
+  locoware = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMW2w63E8Ek6d2uBgsck0/LNbCPWnZNDAmM/EF03sttq";
+  systems = [
+    homepc
+    locoware
+  ];
+in
+{
+  "github-api.age".publicKeys = systems;
+}
