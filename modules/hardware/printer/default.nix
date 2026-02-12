b6a7bec3e42621ae126a8ba85204ecc2a86127d3
Locochoco <contact@locochoco.dev>
2026-02-12 21:47:57 +0100

    Refactoring hardware modules to use new module naming convention

--- modules/hardware/printer/default.nix
+++ modules/hardware/printer/default.nix
@@ -19,16 +19,41 @@ in
           Enables CUPS Printers 
         '';
       };
+      deskjet.enable = mkOption {
+        default = false;
+        type = types.bool;
+        description = ''
+          Enables DeskJet Printers 
+        '';
+      };
     };
   };
 
-  config = mkIf cfg.enable {
-    services.printing.enable = true;
-    services.printing.browsed.enable = true;
-    services.avahi = {
-      enable = true;
-      nssmdns4 = true;
-      openFirewall = true;
-    };
-  };
+  config = mkMerge [
+    (mkIf cfg.enable {
+      services.printing.enable = true;
+      services.printing.browsed.enable = true;
+      services.avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
+    })
+    (mkIf cfg.deskjet.enable {
+      services.printing.drivers = [ pkgs.hplip ];
+      hardware.printers = {
+        ensurePrinters = [
+          {
+            name = "HP_DeskJet_2676";
+            location = "Home";
+            deviceUri = "usb://HP/DeskJet%202600%20series?serial=BR77RFB3NH06P5&interface=1";
+            model = "HP/hp-deskjet_plus_6000_series.ppd.gz";
+            ppdOptions = {
+              PageSize = "A4";
+            };
+          }
+        ];
+      };
+    })
+  ];
 }
