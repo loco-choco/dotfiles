63e9d9ce7c13ef6705957f245b34c302e4cb920a
Locochoco <contact@locochoco.dev>
2026-05-24 22:29:58 +0200

    added ncurses to nixld

--- modules/systems/nix/default.nix
+++ modules/systems/nix/default.nix
@@ -57,6 +57,23 @@ in
     environment.systemPackages = [ pkgs.nix-output-monitor ];
     programs.nix-ld = {
       enable = true;
+      libraries = with pkgs; [
+        zlib
+        zstd
+        stdenv.cc.cc
+        curl
+        openssl
+        attr
+        libssh
+        bzip2
+        libxml2
+        acl
+        libsodium
+        util-linux
+        xz
+        systemd
+        ncurses
+      ];
     };
   };
 }
