2abea6aa80a42e8a0110104752710750b8a2f3da
Locochoco <contact@locochoco.dev>
2026-06-26 18:29:49 +0200

    making nixpkgs accept unfree things

--- hive.nix
+++ hive.nix
@@ -1,6 +1,22 @@
 let
   sources = import ./npins;
-  pkgs = import sources.nixpkgs;
+  pkgs = import sources.nixpkgs {
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "openssl-1.1.1w"
+        "electron-36.9.5"
+        "libxml2-2.13.8"
+        "dotnet-sdk-6.0.428"
+        "dotnet-sdk-7.0.410"
+        "segger-jlink-qt4-874"
+      ];
+      segger-jlink.acceptLicense = true;
+    };
+    overlays = [
+      (import (sources.agenix + "/overlay.nix"))
+    ];
+  };
   lib = pkgs.lib;
 in
 {
