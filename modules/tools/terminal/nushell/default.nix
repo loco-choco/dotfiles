7fb752f99aba8d375c3b97e6b6eccae6f3c7295b
Locochoco <contact@locochoco.dev>
2026-07-01 10:58:34 +0200

    fixed nushell and nix command

--- modules/tools/terminal/nushell/default.nix
+++ modules/tools/terminal/nushell/default.nix
@@ -31,7 +31,6 @@ in
         enable = true;
         package = pkgs.nushell;
         shellAliases = {
-          "nix" = "noglob nix"; # so we can use the # char in flakes
           "w3m" = "w3m -o inline_img_protocol=4"; # so we can see images in kitty
           "den" = "ddgr --np --noua -w en.wiktionary.org";
           "dfr" = "ddgr --np --noua -w fr.wiktionary.org";
