3a25f50c85894cd3f8dafc9b9061c99f5cab313c
Locochoco <contact@locochoco.dev>
2026-07-03 20:40:10 +0200

    removing evil systemd

--- system.nix
+++ system.nix
@@ -1,10 +1,8 @@
 let
   sources = import ./npins;
-  nixpkgs = sources.nixpkgs;
-in import "${nixpkgs}/nixos" {
-  configuration = {
-    ## Overlays and other nixpkgs configs
-    nixpkgs = {
+  finix = import sources.finix;
+  ## Overlays and other nixpkgs configs
+  pkgs = import sources.nixpkgs {
       config = {
         allowUnfree = true;
         permittedInsecurePackages = [
@@ -22,33 +20,40 @@ in import "${nixpkgs}/nixos" {
         (final: _: { nh = final.callPackage (sources.nh + "/package.nix") { }; })
       ];
     };
-    ## Extra modules
-    imports = [
-      (sources.agenix + "/modules/age.nix")
-      (sources.home-manager + "/nixos")
-      (import sources.nvf).nixosModules.default
-      #(import sources.musnix).nixosModules.musnix
-      ./modules/default.nix
-      ### Actual machine configuration
-      ./machines/harpia/configuration.nix
-    ];
-    networking.hostName = "harpia";
-    ## Nixpkgs local pinning 
-    environment.etc.nixpkgs.source = nixpkgs;
-    nix.nixPath = [ 
-      "nixos-config=${toString ./machines/harpia/configuration.nix}"
-      "nixpkgs=/etc/nixpkgs"
-    ];
-    ## Extra cachix caching
-    nix.settings = {
-      extra-substituters = [
-        "https://ow-mods.cachix.org"
-        "https://nix-community.cachix.org"
+in finix.lib.finixSystem {
+  lib = pkgs.lib;
+  modules = [
+    ## Extra Modules
+    (sources.agenix + "/modules/age.nix")
+    (sources.home-manager + "/nixos")
+    (import sources.nvf).nixosModules.default
+    #(import sources.musnix).nixosModules.musnix
+    ## Local Modules 
+    ./modules/default.nix
+    ## Actual machine configuration
+    ./machines/harpia/configuration.nix
+    ## Base Nix/Nixpkgs Configuration
+    {
+      networking.hostName = "harpia";
+      ## Nixpkgs local pinning
+      nixpkgs.pkgs = pkgs;
+      environment.etc.nixpkgs.source = pkgs;
+      nix.nixPath = [ 
+        "nixos-config=${toString ./machines/harpia/configuration.nix}"
+        "nixpkgs=/etc/nixpkgs"
       ];
-      extra-trusted-public-keys = [
-        "ow-mods.cachix.org-1:6RTOd1dSRibA2W0MpZHxzT0tw1RzyhKObTPKQJpcrZo="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
-  };
+      ## Extra cachix caching
+      nix.settings = {
+        extra-substituters = [
+          "https://ow-mods.cachix.org"
+          "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "ow-mods.cachix.org-1:6RTOd1dSRibA2W0MpZHxzT0tw1RzyhKObTPKQJpcrZo="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
+    }
+  ] ## Fenix Modules
+  ++ builtins.attrValues finix.nixosModules;
 }
