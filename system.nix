bc8cf6f0f31cd6451b032452bb1d55396b550cd9
Locochoco <contact@locochoco.dev>
2026-07-13 14:40:23 +0200

    Fixed hardware-configuration to not include magic files and readded finix modules to modules import

--- system.nix
+++ system.nix
@@ -5,6 +5,7 @@ let
   ## Overlays and other nixpkgs configs
   pkgs = import sources.nixpkgs { };
 in finix.lib.finixSystem {
+  lib = pkgs.lib;
   modules = [
     ## Base Laptop Profile
     community-modules.nixosModules.laptop
@@ -15,22 +16,22 @@ in finix.lib.finixSystem {
       networking.hostName = "harpia";
       ## Nixpkgs local pinning
       nixpkgs.pkgs = pkgs;
-      environment.etc.nixpkgs.source = pkgs;
-      nix.nixPath = [ 
-        "nixos-config=${toString ./machines/harpia/configuration.nix}"
-        "nixpkgs=/etc/nixpkgs"
-      ];
-      ## Extra cachix caching
-      nix.settings = {
-        extra-substituters = [
-          "https://ow-mods.cachix.org"
-          "https://nix-community.cachix.org"
-        ];
-        extra-trusted-public-keys = [
-          "ow-mods.cachix.org-1:6RTOd1dSRibA2W0MpZHxzT0tw1RzyhKObTPKQJpcrZo="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
-      };
+      #environment.etc.nixpkgs.source = pkgs;
+      #nix.nixPath = [ 
+      #  "nixos-config=${toString ./machines/harpia/configuration.nix}"
+      #  "nixpkgs=/etc/nixpkgs"
+      #];
+      ### Extra cachix caching
+      #nix.settings = {
+      #  extra-substituters = [
+      #    "https://ow-mods.cachix.org"
+      #    "https://nix-community.cachix.org"
+      #  ];
+      #  extra-trusted-public-keys = [
+      #    "ow-mods.cachix.org-1:6RTOd1dSRibA2W0MpZHxzT0tw1RzyhKObTPKQJpcrZo="
+      #    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      #  ];
+      #};
     }
-  ];
+  ] ++ builtins.attrValues finix.nixosModules;
 }
