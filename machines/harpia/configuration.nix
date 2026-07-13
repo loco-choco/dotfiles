e8240fa869d82058396d051e82dc954c992e09f9
Locochoco <contact@locochoco.dev>
2026-07-13 15:17:35 +0200

    blocked out the configuration basic needs

--- machines/harpia/configuration.nix
+++ machines/harpia/configuration.nix
@@ -9,5 +9,30 @@
   ##              Seat   Manager = seatd
   ##              Wifi   Manager = iwd
   profiles.laptop.hardwareSupport = "minimal";
+
+  ## Hardware Specifics Configuration ##
+
+  ### NVIDIA Drivers (GTX1070)
+
+  ### NVIDIA Prime
+
+  ## Desktop Configuration ##
+
+  ### Niri 
+
+
+  ## Terminal Configuration ##
+
+  ## Neovim Configuration ##
+
+  ## Tunnerls/SSH Configuration ##
+
+  ### Tailscape
+
+  ## Webbrowsing Configuration ##
+
+  ## Gaming Configuration ##
+
+  ## 
 }
 
