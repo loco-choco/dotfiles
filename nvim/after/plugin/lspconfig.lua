38fc08dc514eb178cd17148c68173c642b466ffe
Locochoco <contact@locochoco.dev>
2026-08-16 13:21:22 +0200

    fixed fstar lsp call

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -2,3 +2,7 @@ require('mini.snippets').setup({})
 require('mini.completion').setup({})
 
 vim.lsp.enable({'rust_analyzer', 'csharp_ls', 'fstar'})
+
+vim.lsp.config['fstar'] = {
+  cmd - {'fstar.exe', '--ide'}
+})
